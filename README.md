171674c4b0adb43a2782dd867775a8f61b69c737
Nathan Pham <nathanpham.me@gmail.com>
2022-08-30 20:49:54 -0700

    express integration

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 The next-gen social media made with React and GraphQL except it looks like shit
 
+## Usage
+
+In development
+
+```bash
+npm run dev         # creates nodemon instance to easily change GraphQL backend
+npm run dev:client  # init dev vite server
+```
+
+In production
+
+```bash
+npm run build       # automatically build & serve frontend
+```
+
 ## Screenshots
 
 ![](2022-08-30-20-29-17.png)
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
+const {
+    ApolloServerPluginDrainHttpServer,
+    ApolloServerPluginLandingPageLocalDefault,
+} = require("apollo-server-core");
+
 const mongoose = require("mongoose");
 const gql = require("graphql-tag");
+
+const express = require("express");
+const http = require("http");
+const path = require("path");
 
 const { UserTypes, UserQuery, UserMutation } = require("./schema/user");
 const { PostTypes, PostQuery, PostMutation } = require("./schema/post");
@@ -30,15 +39,41 @@ const resolvers = {
     },
 };
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req }) => ({ req }),
-});
+const startApolloServer = async (port = 5500) => {
+    const app = express();
 
-mongoose
-    .connect(
+    app.use(express.static("./client/dist"));
+    app.get("/", (_, res) => {
+        res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+    });
+
+    const httpServer = http.createServer(app);
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: ({ req }) => ({ req }),
+        csrfPrevention: true,
+        cache: "bounded",
+        plugins: [
+            ApolloServerPluginDrainHttpServer({ httpServer }),
+            ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+        ],
+    });
+
+    // connect to database
+    await mongoose.connect(
         `mongodb+srv://phamn23:${process.env.MONGO_PASSWORD}@cluster0.idsmesd.mongodb.net/?retryWrites=true&w=majority`
-    )
-    .then(() => server.listen({ port: 5500 }))
-    .then(({ url }) => `🚀 Server ready at ${url}`);
+    );
+
+    // start apollo server
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    httpServer.listen({ port }, () => {
+        console.log(
+            `🚀 Server ready at http://localhost:${port}${apolloServer.graphqlPath}`
+        );
+    });
+};
+
+startApolloServer();
